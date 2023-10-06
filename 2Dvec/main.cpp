7b1b11be35cf2e407ad1507ebf85b643f60d2768
pscieszka <piotr.scieszka.epinote@gmail.com>
2023-10-06 20:46:21 +0200

    init

---
+++ 2Dvec/main.cpp
@@ -0,0 +1,23 @@
+class vec{
+private:
+    int x;
+    int y;
+public:
+
+void setX(int x){
+    this->x = x;
+}
+
+void setY(int y){
+    this->y = y;
+}
+
+int getX(){
+    return x;
+}
+
+int getY(){
+    return y;
+}
+
+};
